57af918f39cb9918d870b66ecb5e1ccedf4fdfc6
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-08-28 14:33:53 -0300

    add pg

--- functions/src/models/db.js
+++ functions/src/models/db.js
@@ -3,17 +3,18 @@ const { Sequelize } = require('sequelize')
 const sequelize = new Sequelize(
   'postgres://woxmhyfp:XckR6ybQzIz6DtNZqX_dGzCpUlVaF8XB@tuffi.db.elephantsql.com/woxmhyfp',
   {
-    dialect: 'pg',
+    dialect: 'postgres',
+    dialectModule: require('pg'),
   }
 )
 
 sequelize
   .authenticate()
   .then(() => {
-    console.log('Connection has been established successfully.')
+    console.log('conectado com sucesso!.')
   })
   .catch(err => {
-    console.error('Unable to connect to the database:', err)
+    console.error('erro ao conectar ao database:', err)
   })
 
 module.exports = sequelize
