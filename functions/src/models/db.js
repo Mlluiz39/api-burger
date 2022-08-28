acd4de89ebb0322f618124945b365e2d45ef9c4d
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-08-28 14:07:42 -0300

    add pg

--- functions/src/models/db.js
+++ functions/src/models/db.js
@@ -1,10 +1,10 @@
-import * as pg from 'pg'
 const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize(
-  'postgres://woxmhyfp:XckR6ybQzIz6DtNZqX_dGzCpUlVaF8XB@tuffi.db.elephantsql.com/woxmhyfp', {
-    dialect: 'pg',
-}
+  'postgres://woxmhyfp:XckR6ybQzIz6DtNZqX_dGzCpUlVaF8XB@tuffi.db.elephantsql.com/woxmhyfp',
+  {
+    dialect: 'postgres',
+  }
 )
 
 sequelize
