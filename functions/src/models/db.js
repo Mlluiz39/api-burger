8fbd004fbea7ba8d4077a61a9aa9e7ea3f1eb8ae
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-08-28 14:10:26 -0300

    add pg dialect

--- functions/src/models/db.js
+++ functions/src/models/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize')
 const sequelize = new Sequelize(
   'postgres://woxmhyfp:XckR6ybQzIz6DtNZqX_dGzCpUlVaF8XB@tuffi.db.elephantsql.com/woxmhyfp',
   {
-    dialect: 'postgres',
+    dialect: 'pg',
   }
 )
 
