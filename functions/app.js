3f70ff9a4f3ed3f1e5d6c5f20248c2fc76ad0882
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-08-28 17:04:19 -0300

    add

--- functions/app.js
+++ functions/app.js
@@ -12,19 +12,14 @@ const router = express.Router()
 
 app.use(express.json())
 app.use('/', router)
-app.use(cors())
-app.use((req, res, next) => {
-  res.header(
-    'Access-Control-Allow-Headers, *, Access-Control-Allow-Origin',
-    'Origin, X-Requested-with, Content_Type,Accept,Authorization',
-    'https://mlluizpereira.ml/'
-  )
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET')
-    return res.status(200).json({})
-  }
-  next()
-})
+app.use(
+  cors({
+    origin: ['https://mlluizpereira.ml/'],
+    methods: ['GET', 'POST', 'DELETE'],
+    credentials: true,
+    origin: true,
+  })
+)
 
 router.get('/order', async (req, res) => {
   const order = await Order.findAll()
