0967979723e4d798b57ca9c43c3a7cb573d774b0
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-08-28 16:54:44 -0300

    add cors

--- functions/app.js
+++ functions/app.js
@@ -13,6 +13,18 @@ const router = express.Router()
 app.use(express.json())
 app.use('/', router)
 app.use(cors())
+app.use((req, res, next) => {
+  res.header(
+    'Access-Control-Allow-Headers, *, Access-Control-Allow-Origin',
+    'Origin, X-Requested-with, Content_Type,Accept,Authorization',
+    'http://localhost:4200'
+  )
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,PATCH,DELETE,GET')
+    return res.status(200).json({})
+  }
+  next()
+})
 
 router.get('/order', async (req, res) => {
   const order = await Order.findAll()
