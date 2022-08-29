582ea4e3995f628ec8fa9bef4e3f29d0b0681303
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-08-29 18:23:01 -0300

    add app in main paste

--- app.js
+++ app.js
@@ -3,39 +3,24 @@ const cors = require('cors')
 const serverless = require('serverless-http')
 
 const app = express()
-const port = process.env.PORT || 3001
-require('dotenv/config');
 
-<<<<<<< HEAD
-=======
-
->>>>>>> parent of b38b247 (remove port)
-const Order = require('./src/models/Order')
+const Order = require('./functions/src/models/Order')
 
 const router = express.Router()
 
 app.use(express.json())
 app.use(cors())
-<<<<<<< HEAD
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
-})
 
 router.get('/order', async (req, res) => {
   const order = await Order.findAll()
   return res.json(order)
-=======
+})
 app.use('/', router)  
 
 router.get('/order', async (req, res) => {
  const orders = await Order.findAll()
   return res.json(orders)
->>>>>>> parent of b38b247 (remove port)
+
 })
 
 router.get('/order/:id', async (req, res) => {
