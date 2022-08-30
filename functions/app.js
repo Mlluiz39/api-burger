a1196983aadafeba8a1f79bc29252227e64b7c59
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-08-30 17:40:09 -0300

    add cors

--- functions/app.js
+++ functions/app.js
@@ -9,14 +9,12 @@ const Order = require('./src/models/Order')
 const router = express.Router()
 
 app.use(express.json())
-app.use(
-  cors({
-    origin: ['https://mlluizpereira.ml'],
-    methods: ['GET', 'POST', 'DELETE'],
-    credentials: true,
-    origin: true,
-  })
-)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+  app.use(cors())
+  next()
+})
 
 router.get('/order', async (req, res) => {
   const order = await Order.findAll()
@@ -24,9 +22,8 @@ router.get('/order', async (req, res) => {
 })
 
 router.get('/order', async (req, res) => {
- const orders = await Order.findAll()
+  const orders = await Order.findAll()
   return res.json(orders)
-
 })
 
 router.get('/order/:id', async (req, res) => {
@@ -35,22 +32,31 @@ router.get('/order/:id', async (req, res) => {
 })
 
 router.post('/order', async (req, res) => {
- const { firstName, hamburger, drink, follow_up } = req.body
-  const order = await Order.create({ id: Order.id, firstName, hamburger, drink, follow_up })
+  const { firstName, hamburger, drink, follow_up } = req.body
+  const order = await Order.create({
+    id: Order.id,
+    firstName,
+    hamburger,
+    drink,
+    follow_up,
+  })
   return res.json(order)
 })
 
 router.put('/order/:id', async (req, res) => {
- const { firstName, hamburger, drink, follow_up } = req.body
-  const order = await Order.update({ firstName, hamburger, drink, follow_up }, { where: { id: req.params.id } })
+  const { firstName, hamburger, drink, follow_up } = req.body
+  const order = await Order.update(
+    { firstName, hamburger, drink, follow_up },
+    { where: { id: req.params.id } }
+  )
   return res.json(order)
 })
 
 router.delete('/order/:id', async (req, res) => {
-    const order = await Order.destroy({ where: { id: req.params.id } })
-    return res.json(order)
-  })
+  const order = await Order.destroy({ where: { id: req.params.id } })
+  return res.json(order)
+})
 
-  app.use('/', router)
+app.use('/', router)
 
 module.exports.handler = serverless(app)
