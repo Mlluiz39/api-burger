6ee53f9dfa132c588c9b523619e1dbb223291090
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-09-04 18:24:01 -0300

    add new db

--- functions/src/models/db.js
+++ functions/src/models/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('node-burger-api', 'postgres', 'Julia2912@', {
-  host: '140.238.183.195',
+const sequelize = new Sequelize('node-order', 'mlluiz', 'Julia2912@', {
+  host: '150.230.80.167',
   dialect: 'postgres',
   dialectModule: require('pg'),
 })
