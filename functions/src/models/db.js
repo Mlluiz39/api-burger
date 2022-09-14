c95fd2b3166fb7fb8d66e1733532ab441f700648
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-09-14 18:25:39 -0300

    add new database

--- functions/src/models/db.js
+++ functions/src/models/db.js
@@ -1,10 +1,8 @@
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize('node-order', 'mlluiz', 'Julia2912@', {
-  host: '150.230.80.167',
-  dialect: 'postgres',
-  dialectModule: require('pg'),
-})
+const sequelize = new Sequelize(
+  'postgres://woxmhyfp:XckR6ybQzIz6DtNZqX_dGzCpUlVaF8XB@tuffi.db.elephantsql.com/woxmhyfp'
+)
 
 sequelize
   .authenticate()
