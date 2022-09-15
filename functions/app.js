c9049fff457db3d6958a88f093e0e22958303ba8
Marcelo Luiz <mlluizpereira39@gmail.com>
2022-09-15 10:31:41 -0300

    remove allow cors

--- functions/app.js
+++ functions/app.js
@@ -12,16 +12,6 @@ const router = express.Router()
 app.use(express.json())
 app.use(cors())
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  )
-  next()
-})
-
 router.get('/order', async (req, res) => {
   const order = await Order.findAll()
   return res.json(order)
